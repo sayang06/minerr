b794c2f69180557cc31c932007e87ee1d572a401
sayang06 <78871900+sayang06@users.noreply.github.com>
2021-06-30 17:48:43 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,4 @@
-#veruscoin
+# veruscoin
 sudo apt update
 wget https://github.cwget wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
